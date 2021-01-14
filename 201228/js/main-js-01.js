39c97f478bbb9696445a386352bb1e3e11fa7b46
yoonyj1103 <77230019+yoonyj1103@users.noreply.github.com>
2021-01-14 09:43:03 +0900

    1
    1

---
+++ 201228/js/main-js-01.js
@@ -0,0 +1,50 @@
+let a='10';
+let b='+';
+
+// document.write(a+b+a+"<br>");
+
+let num01=100;
+let num02=3;
+let br="<br>";
+let h1="<h1>";
+let h_1="</h1>";
+
+document.write(h1+num01+num02+h_1+br);
+// 103
+document.write(num01-num02+"<br>");
+// 97
+document.write(num01*num02+"<br>");
+// 300
+document.write(num01/num02+"<br>");
+// 33.3333
+document.write(num01%num02+"<br>");
+// 1
+
+
+// let num01=50;
+// let num02=40;
+// document.write(num01+num02+"<br>");
+
+// document.write(a+num01+"<br>");
+// 10문자열 50은 숫자
+
+// 연산자
+// +, -, /, *, %
+
+// var a=1;
+// var b='2';
+// var c="안녕?";
+
+// console.log(a) ;
+// // 테스팅 용도
+// document.write(a);
+// // 바로출력
+
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+// 1. use strict
+// added in ES5
+// use this for Valina Javascript
+
+'use strict';
